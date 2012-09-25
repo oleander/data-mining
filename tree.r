a6971a0d33e61fea150a67ed757164203bc50407
Linus Oleander <linus@oleander.nu>
2012-09-25 12:28:04 +0200

    First draft of bestsplit function

--- tree.r
+++ tree.r
@@ -14,17 +14,29 @@ tree.impurity = function (v) {
 }
 
 tree.bestsplit = function (x, y) {
-	x_ = x[order(x)]
-	y_ = y[order(x)]
-	
-	for (a in unique(x)) {
-		
-		left = y_[x_ < a]
-		right = y_[x_ >= a]
+  x_ <- x[order(x)]
+  y_ <- y[order(x)]
 
-		impurity(left)
-		
-		impurity(right)
-		
-	}
+  bestR = -1
+  bestS = NULL  
+
+  for (i in 1:  (length(x_) - 1))  {
+    if (x_[i] == x_[i + 1]) {
+      next
+    }
+
+    a <- mean(x_[i]:x_[i + 1])
+    left = y_[x_ <= a]
+    right = y_[x_ > a]
+    piLeft <- length(left)/length(x_)
+    piRight <- length(right)/length(x_)
+
+    currentR = impurity(x) - (piLeft*impurity(left) + piRight*impurity(right))
+    if(currentR > bestR){
+      bestR = currentR
+      bestS = a
+    }
+  }
+
+  return(bestS)
 }

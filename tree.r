a0659f2ad9a01ab5ce8f9b19f6d7391fd134e36e
jvdp <jvdplas@gmail.com>
2012-10-09 16:31:15 +0200

    reordered functions a bit classify bug fix pima confusion table function

--- tree.r
+++ tree.r
@@ -1,46 +1,3 @@
-#
-# @xs Array<Array<Integer>>
-# @tr A custom tree
-#
-tree.classify = function(cases, tr) {
-  classes = c()
-  for (i in 1:(nrow(cases))) {
-    classes[i] = tree.calcClassify(cases[i,], tr)
-  }
-  return(classes)
-}
-
-tree.calcClassify = function(case, tr) {
-  if(tree.isNode(tr)){
-    index = tr[[3]][[3]]
-    comp = case[index]
-
-    # Walk left
-    if(comp <= tr[[4]]){
-      return(tree.calcClassify(case, tr[[5]]))
-    # Walk right
-    } else {
-      return(tree.calcClassify(case, tr[[6]]))
-    }
-  } else if(tree.isLeaf(tr)){
-    if (tr[1] > tr[2]) {
-      return(0)
-    } else {
-      return(1)
-    }
-  } else {
-    print("\n\nWTF")
-  }
-}
-
-tree.isNode = function(tr) {
-  return(length(tr) == 6)
-}
-
-tree.isLeaf = function(tr) {
-  return(length(tr) == 2)
-}
-
 tree.grow = function(matrix, class, nmin=2, minleaf=1) {
   nClass0 = length(which(class == 0))
   nClass1 = length(which(class == 1))
@@ -79,6 +36,76 @@ tree.grow = function(matrix, class, nmin=2, minleaf=1) {
   )
 }
 
+#
+# @xs Array<Array<Integer>>
+# @tr A custom tree
+#
+tree.classify = function(cases, tr) {
+  classes = c()
+  for (i in 1:(nrow(cases))) {
+    classes[i] = tree.calcClassify(cases[i,], tr)
+  }
+  return(classes)
+}
+
+tree.calcClassify = function(case, tr) {
+  if(tree.isNode(tr)){
+    index = tr[[3]]
+    comp = case[index]
+
+    # Walk left
+    if(comp <= tr[[4]]){
+      return(tree.calcClassify(case, tr[[5]]))
+    # Walk right
+    } else {
+      return(tree.calcClassify(case, tr[[6]]))
+    }
+  } else if(tree.isLeaf(tr)){
+    if (tr[[1]] > tr[[2]]) {
+      return(0)
+    } else {
+      return(1)
+    }
+  } else {
+    print("\n\nWTF")
+  }
+}
+
+tree.pimaConfusion = function(nmin = 20, minleaf = 5) {
+  data = read.csv('pima.txt', header = FALSE)
+  classes = data[,9]
+  tree   = tree.grow(data[,1:8], classes, nmin, minleaf)
+  classes_ = tree.classify(data[,1:8], tree)
+
+  matrix = matrix(c(
+    length(classes[classes == 0 & classes_ == 0]),
+    length(classes[classes == 1 & classes_ == 0]),
+    length(classes[classes == 0 & classes_ == 1]),
+    length(classes[classes == 1 & classes_ == 1])
+  ), 2)
+  return(matrix)
+}
+
+tree.main = function() {
+  matrix = read.csv('credit.txt')
+  small = tree.grow(matrix[,1:5], matrix[,6])
+
+  return(small)
+
+  matrix = read.csv('pima.txt', header = FALSE)
+  large = tree.grow(matrix[,1:8], matrix[,9])
+
+  return(c(small, large))
+}
+
+tree.isNode = function(tr) {
+  return(length(tr) == 6)
+}
+
+tree.isLeaf = function(tr) {
+  return(length(tr) == 2)
+}
+
 tree.print = function(node, level = 0) {
   indent = sprintf(paste0("%", level, "s"), "")
   if(tree.isNode(node)) {
@@ -89,18 +116,6 @@ tree.print = function(node, level = 0) {
     cat(indent, sprintf("Leaf: (%s|%s)", node[1], node[2]), "\n")
   }
 
-}
-
-tree.main = function() {
-  matrix = read.csv('credit.txt')
-  small = tree.grow(matrix[,1:5], matrix[,6])
-
-  return(small)
-
-  matrix = read.csv('pima.txt')
-  large = tree.grow(matrix[,1:8], matrix[,9])
-
-  return(c(small, large))
 }
 
 tree.impurity = function (v) {

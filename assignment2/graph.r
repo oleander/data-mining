26da7eda729116cd8e31ba9580c625a9a2b24738
Linus Oleander <linus@oleander.nu>
2012-10-23 16:19:08 +0200

    First draft of bk implmentation

--- assignment2/graph.r
+++ assignment2/graph.r
@@ -4,10 +4,31 @@
 # [3,]    1    1    0    1
 # [4,]    0    0    1    0
 
-graph.init = matrix(c(0,1,1,0,1,0,1,0,1,1,0,1,0,0,1,0), 4, 4)
+# graph.init = matrix(c(
+#   0,1,1,0,
+#   1,0,1,0,
+#   1,1,0,1,
+#   0,0,1,0
+# ), 4, 4)
+
+#      [,1] [,2] [,3] [,4] [,5]
+# [1,]    0    1    1    0    1
+# [2,]    1    0    0    1    0
+# [3,]    1    0    0    1    1
+# [4,]    0    1    1    0    0
+# [5,]    1    0    1    0    0
+graph.init = matrix(c(
+  0,1,1,0,1,1,0,0,1,0,1,0,0,1,1,0,1,1,0,0,1,0,1,0,0
+), 5, 5)
+
+graph.result = list()
 
 n = function(v) {
-  return(which(graph.init[v,] == 1, arr.in=TRUE))
+  which(graph.init[v,] == 1, arr.in=TRUE)
+}
+
+calcC = function(matrix) {
+  bk(c(), 1:nrow(matrix), c())
 }
 
 # BronKerbosch1(R,P,X):
@@ -17,17 +38,16 @@ n = function(v) {
 #         BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
 #         P := P \ {v}
 #         X := X ⋃ {v}
-bk = function(R,P,X) {
-  print(P)
-  print(X)
-  if(nrow(P) == 0 && length(X) == 0){
-    return(X)
+bk = function(R,P,X, res = list()) {
+  if(length(P) == 0 && length(X) == 0){
+    graph.result[[length(graph.result) + 1]] = R
+    print(R)
+    return
   }
-  
-  for (v in 1:nrow(P)) {
-    # bk(c(R, v), intersect(P, n(v)), intersect(X, n(v)))
-    bk(c(R, v), P[n(v)-v,], intersect(X, n(v)))
-    P = P[-1,]
-    X = c(C, v)
+
+  for (v in P) {
+    bk(union(R, v), intersect(P, n(v)), intersect(X, n(v)))
+    P = P[-1]
+    X = union(X, v)
   }
 }
